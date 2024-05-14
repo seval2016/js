ab6cc3b82429cf9aa6211ef554ebc8549be2cc3c
seval2016 <sevalsenturk2016@gmail.com>
2024-05-15 01:31:37 +0300

    style.js buton design

---
+++ lord-of-the-ring/style.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sayfa yüklendiğinde localStorage'dan counter değerlerini al ve ekrana yaz
+    document.querySelectorAll(".counter").forEach(function(counterElement, index) {
+        var key = "counter_" + index;
+        var count = localStorage.getItem(key);
+        if (count !== null) {
+            counterElement.textContent = count;
+        }
+    });
+
+    // Message, like ve follow butonlarına tıklama olaylarını dinle
+    document.getElementById("messageButton").addEventListener("click", function() {
+        updateCounter(0);
+    });
+
+    document.getElementById("likeButton").addEventListener("click", function() {
+        updateCounter(1);
+    });
+
+    document.getElementById("followButton").addEventListener("click", function() {
+        updateCounter(2);
+    });
+
+    // Counter değerini artırma ve localStorage'a kaydetme fonksiyonu
+    function updateCounter(index) {
+        var counterElement = document.querySelectorAll(".right-box")[index].querySelector(".counter");
+        var currentCount = parseInt(counterElement.textContent);
+        currentCount++;
+        counterElement.textContent = currentCount;
+        var key = "counter_" + index;
+        localStorage.setItem(key, currentCount);
+    }
+
+
+    document.getElementById("reset").addEventListener("click", function() {
+        // Tüm counter değerlerini sıfırla
+        document.querySelectorAll(".counter").forEach(function(counterElement, index) {
+            counterElement.textContent = 0;
+            var key = "counter_" + index;
+            localStorage.setItem(key, 0);
+        });
+    });
+});
